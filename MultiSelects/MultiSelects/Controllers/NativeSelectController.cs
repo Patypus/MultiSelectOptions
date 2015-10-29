6378f0845cd59068d27a0f8b668a105a6590aab3
Pat Howell <patrick.howell@outlook.com>
2015-10-29 22:06:30 +0000

    Started adding Multiselects view

---
+++ MultiSelects/MultiSelects/Controllers/NativeSelectController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MultiSelects.Controllers
+{
+    public class NativeSelectController : Controller
+    {
+        public ViewResult Display()
+        {
+            return View();
+        }
+    }
+}
---
+++ MultiSelects/MultiSelects/DataLayer/ServerFacade.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MultiSelects.DataLayer
+{
+    public class ServerFacade
+    {
+    }
+}
