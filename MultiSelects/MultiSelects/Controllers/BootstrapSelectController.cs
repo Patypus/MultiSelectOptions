7f76b9c64073c2bb3c167f9c53817700bb7f59af
Pat Howell <patrick.howell@outlook.com>
2016-03-16 22:12:46 +0000

    Started adding base for bootstrap selects

---
+++ MultiSelects/MultiSelects/Controllers/BootstrapSelectController.cs
@@ -0,0 +1,17 @@
+﻿using MultiSelects.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MultiSelects.Controllers
+{
+    public class BootstrapSelectController : Controller
+    {
+        public ViewResult Display(VisitedLocationsDisplayViewModel model)
+        {
+            return View(model);
+        }
+    }
+}
